6bc670143c49bea442fdede5ec849d731c89ac42
Al-Amin Hossain <alaminhossain.tech@gmail.com>
2024-02-03 09:45:38 +0600

    complete asked changes

--- custom/assets/paeds/scripts/main.js
+++ custom/assets/paeds/scripts/main.js
@@ -140,8 +140,3 @@ $(function () {
     }
   });
 });
-
-// window.addEventListener("load", function () {
-//   document.getElementById("preloader").style.display = "none";
-//   document.getElementById("content").classList.remove("hidden");
-// });
